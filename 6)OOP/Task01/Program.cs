7c0ccd6d15fe6a5b0cb4d3147060fca4cc2c2d2b
Iliays <blacking228@gmail.com>
2022-04-15 19:16:30 +0500

    OOP

---
+++ 6)OOP/Task01/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+/*
+ * Создать класс игрока, с полями, содержащими информацию об игроке и методом, который выводит информацию на экран.
+ * В классе обязательно должен быть конструктор
+ */
+
+namespace Task01
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Player player1 = new Player("Killer", 100, 30);
+			player1.ShowInfo();
+		}
+	}
+
+	class Player
+	{
+		private string _name;
+		private int _health;
+		private int _mana;
+
+		public Player(string name, int health, int mana)
+		{
+			_name = name;
+			_health = health;
+			_mana = mana;
+		}
+
+		public void ShowInfo()
+		{
+			Console.WriteLine($"Игрок по имени: {_name}\nХП: {_health} Мана: {_mana}");
+		}
+	}
+}
---
+++ 6)OOP/Task02/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+/*
+ * Создать класс игрока, у которого есть поля с его положением в x,y.
+ * Создать класс отрисовщик, с методом, который отрисует игрока.
+ * Попрактиковаться в работе со свойствами.
+ */
+
+namespace Task02
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Player player1 = new Player(5, 10);
+			Renderer renderer = new Renderer();
+			renderer.DrawPlayer(player1.XPlayerPosition, player1.YPlayerPosition);
+		}
+	}
+
+	class Player
+	{
+		public int XPlayerPosition { get; private set; }
+		public int YPlayerPosition { get; private set; }
+
+		public Player(int x, int y)
+		{
+			XPlayerPosition = x;
+			YPlayerPosition = y;
+		}
+	}
+
+	class Renderer
+	{
+		public void DrawPlayer(int x, int y, char symbol = '$')
+		{
+			Console.SetCursorPosition(x, y);
+			Console.Write(symbol);
+		}
+	}
+}
